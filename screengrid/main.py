5b91a372131d155164173378d7a37319b7d1a5cb
Evan <evfredericksen@gmail.com>
2018-05-19 22:07:59 -0400

    basic grid

--- screengrid/main.py
+++ screengrid/main.py
@@ -1,6 +1,10 @@
 import win32api, win32con, win32gui, win32ui
 import threading
 import time
+import string
+import ctypes
+
+import screencanvas
 
 windowText = 'a foo barred'
 
@@ -13,68 +17,35 @@ def customDraw(hWindow):
     # win32gui.CloseWindow(hWindow)
 
 def main():
-    hInstance = win32api.GetModuleHandle()
-    className = 'MyWindowClassName'
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/ms633576(v=vs.85).aspx
-    # win32gui does not support WNDCLASSEX.
-    wndClass                = win32gui.WNDCLASS()
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/ff729176(v=vs.85).aspx
-    wndClass.style          = win32con.CS_HREDRAW | win32con.CS_VREDRAW
-    wndClass.lpfnWndProc    = wndProc
-    wndClass.hInstance      = hInstance
-    wndClass.hCursor        = win32gui.LoadCursor(None, win32con.IDC_ARROW)
-    wndClass.hbrBackground  = win32gui.GetStockObject(win32con.WHITE_BRUSH)
-    wndClass.lpszClassName  = className
-    # win32gui does not support RegisterClassEx
-    wndClassAtom = win32gui.RegisterClass(wndClass)
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
-    # Consider using: WS_EX_COMPOSITED, WS_EX_LAYERED, WS_EX_NOACTIVATE, WS_EX_TOOLWINDOW, WS_EX_TOPMOST, WS_EX_TRANSPARENT
-    # The WS_EX_TRANSPARENT flag makes events (like mouse clicks) fall through the window.
-    exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/ms632600(v=vs.85).aspx
-    # Consider using: WS_DISABLED, WS_POPUP, WS_VISIBLE
-    style = win32con.WS_DISABLED | win32con.WS_POPUP | win32con.WS_VISIBLE
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/ms632680(v=vs.85).aspx
-    hWindow = win32gui.CreateWindowEx(
-        exStyle,
-        wndClassAtom,
-        None, # WindowName
-        style,
-        0, # x
-        0, # y
-        win32api.GetSystemMetrics(win32con.SM_CXSCREEN), # width
-        win32api.GetSystemMetrics(win32con.SM_CYSCREEN), # height
-        None, # hWndParent
-        None, # hMenu
-        hInstance,
-        None # lpParam
-    )
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/ms633540(v=vs.85).aspx
-    win32gui.SetLayeredWindowAttributes(hWindow, 0x00ffffff, 255, win32con.LWA_COLORKEY | win32con.LWA_ALPHA)
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/dd145167(v=vs.85).aspx
-    #win32gui.UpdateWindow(hWindow)
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/ms633545(v=vs.85).aspx
-    win32gui.SetWindowPos(hWindow, win32con.HWND_TOPMOST, 0, 0, 0, 0,
-        win32con.SWP_NOACTIVATE | win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx
-    #win32gui.ShowWindow(hWindow, win32con.SW_SHOW)
-    threading.Thread(target=customDraw, args=(hWindow,)).start()
-    win32gui.PumpMessages()
+    canvas = screencanvas.ScreenCanvas()
+    letters = string.ascii_lowercase
+    xsize = canvas.width // len(letters)
+    xremainder = canvas.width % len(letters) 
+    ysize = canvas.height // len(letters)
+    yremainder = canvas.height % len(letters)
+    y = 0
+    for i, row_letter in enumerate(letters):
+        x = 0
+        for j, col_letter in enumerate(letters):
+            canvas.add_rectangle(x, y, xsize, ysize, f'{row_letter}{col_letter}')
+            x += xsize
+            if j < xremainder:
+                x += 1
+        y += ysize 
+        if i < yremainder:
+            y += 1
+    canvas.render()
+    s = time.time()
+    while time.time() - s < 10:
+        win32gui.PumpWaitingMessages()
+    print('dun')
 
 def wndProc(hWnd, message, wParam, lParam):
     if message == win32con.WM_PAINT:
         hdc, paintStruct = win32gui.BeginPaint(hWnd)
 
         dpiScale = win32ui.GetDeviceCaps(hdc, win32con.LOGPIXELSX) / 60.0
-        fontSize = 80
+        fontSize = 20
 
         # http://msdn.microsoft.com/en-us/library/windows/desktop/dd145037(v=vs.85).aspx
         lf = win32gui.LOGFONT()
@@ -87,6 +58,7 @@ def wndProc(hWnd, message, wParam, lParam):
         win32gui.SelectObject(hdc, hf)
 
         rect = win32gui.GetClientRect(hWnd)
+        _, _, width, height = win32gui.GetClientRect(hWnd)
         # http://msdn.microsoft.com/en-us/library/windows/desktop/dd162498(v=vs.85).aspx
         win32gui.DrawText(
             hdc,
--- screengrid/region.py
+++
@@ -1,7 +0,0 @@
-class Region:
-
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
