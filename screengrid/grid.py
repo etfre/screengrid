8804605cc9aeec46d44fea273a4ab8d4103bce7f
Evan <evfredericksen@gmail.com>
2018-05-27 15:26:11 -0400

    pypi setup

--- screengrid/grid.py
+++ screengrid/grid.py
@@ -1,5 +1,16 @@
 import string
+import keyboard
 import screencanvas
+
+import win32api, win32con, win32gui, win32ui
+import threading
+import time
+import string
+import ctypes
+import functools
+import mouse
+
+LETTERS = set(string.ascii_lowercase)
 
 class Grid:
 
@@ -7,13 +18,35 @@ class Grid:
         self.canvas = screencanvas.ScreenCanvas()
         self.selection = ''
         self.centers = {}
+        self.keyboard_hook = None
 
     def reset(self):
         self.canvas.reset()
         self.centers = {}
+        try:
+            keyboard.unhook(self.keyboard_hook)
+        except KeyError:
+            pass
+
+    def _on_key_press(self, key):
+        if key.event_type == 'down':
+            return
+        if key.name in LETTERS:
+            if len(self.selection) == 1:
+                x, y = self.centers[f'{self.selection}{key.name}']
+                mouse.move(x, y)
+                self.empty()
+            else:
+                self.draw_letter_grid(row=key.name)
+                self.selection += key.name
+        elif key.name == 'backspace':
+            self.draw_letter_grid()
+        elif key.name == 'esc':
+            self.empty()
 
     def draw_letter_grid(self, row=None):
         self.reset()
+        self.keyboard_hook = keyboard.hook(self._on_key_press, suppress=True)
         letters = string.ascii_lowercase
         xsize = self.canvas.width // len(letters)
         xremainder = self.canvas.width % len(letters) 
--- screengrid/main.py
+++ screengrid/main.py
@@ -14,32 +14,16 @@ done = False
 
 LETTERS = set(string.ascii_lowercase)
 
-def on_press(main_grid, key):
-    global done
-    if key.event_type == 'down':
-        return
-    if key.name in LETTERS:
-        if len(main_grid.selection) == 1:
-            x, y = main_grid.centers[f'{main_grid.selection}{key.name}']
-            mouse.move(x, y)
-            main_grid.empty()
-        else:
-            main_grid.draw_letter_grid(row=key.name)
-            main_grid.selection += key.name
-    elif key.name == 'backspace':
-        main_grid.draw_letter_grid()
-    elif key.name == 'esc':
-        main_grid.empty()
-
-def main():
+def foo():
     main_grid = grid.Grid()
     main_grid.draw_letter_grid()
-    hook = keyboard.hook(functools.partial(on_press, main_grid), suppress=True)
     s = time.time()
-    while time.time() - s < 10:
+    while time.time() - s < 100:
         win32gui.PumpWaitingMessages()
-    keyboard.unhook_all()
-    print('dun')
+
+def main():
+    threading.Thread(target=foo, daemon=True).start()
+    input()
 
 if __name__ == '__main__':
     main()
